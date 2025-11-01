4187c42368136734817629e4e4b3f811d265d1d1
Ricardo Avila <ricardoavilabazalar@gmail.com>
2025-11-01 17:14:51 +0100

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repositorio_Aprendizaje_-Platzi_paquetePip",    # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Ricardo Avila",                         # Tu nombre
+    author_email="ricardo@avila.ws",                 # Tu correo electrónico
+    url="https://github.com/rikrd19/Repositorio_Aprendizaje_-Platzi_paquetePip",     # URL del proyecto
+)
